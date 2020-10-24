b636b8833ca01e20bcb8f7599359a30b4c6ccd63
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2020-10-24 20:00:01 +0100

    Refactored index file. Created service workers to track checks

--- index.js
+++ index.js
@@ -4,108 +4,23 @@
  */
 
 //Dependencies
-const http = require("http");
-const https = require("https");
-const fs = require("fs");
-var url = require("url");
-var StringDecoder = require("string_decoder").StringDecoder;
-var config = require("./config");
-var handlers = require("./lib/handlers");
-var helpers = require("./lib/helpers");
+var server = require("./lib/server");
+var workers = require("./lib/workers");
 
-// helpers.sendTwilioSms(
-//   "8106723916",
-//   "Hello There. this is a test sms",
-//   function (err) {
-//     console.log("This was the error: ", err);
-//   }
-// );
-//Instantiate the HTTP server
-var httpServer = http.createServer(function (req, res) {
-  unifiedServer(req, res);
-});
+// Declare the app
+var app = {};
 
-//Start the HTTP server
-httpServer.listen(config.httpPort, function () {
-  console.log("This server is listen on port " + config.httpPort);
-});
+// Init function
+app.init = function () {
+  // Start the server
+  server.init();
 
-//Instantiate the HTTPS server
-var httpsServerOptions = {
-  key: fs.readFileSync("./https/key.pem"),
-  cert: fs.readFileSync("./https/cert.pem"),
-};
-var httpsServer = https.createServer(httpsServerOptions, function (req, res) {
-  unifiedServer(req, res);
-});
-
-//Start the HTTPs server
-httpsServer.listen(config.httpsPort, function () {
-  console.log("This server is listen on port " + config.httpsPort);
-});
-
-//All the server logic for both the http and https
-var unifiedServer = function (req, res) {
-  //Parse the URL
-  var parsedURL = url.parse(req.url, true);
-
-  //Get path
-  var path = parsedURL.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
-
-  //Get the query string object
-  var queryStringObject = parsedURL.query;
-
-  //Get the headers as an object
-  var headers = req.headers;
-
-  //Get the HTTP method
-  var method = req.method.toLowerCase();
-
-  //Get the payload if any
-  var decoder = new StringDecoder("utf-8");
-  var buffer = "";
-  req.on("data", function (data) {
-    buffer += decoder.write(data);
-  });
-  req.on("end", function () {
-    buffer += decoder.end();
-
-    var chosenHandler =
-      typeof router[trimmedPath] !== "undefined"
-        ? router[trimmedPath]
-        : handlers.notFound;
-
-    var data = {
-      trimmedPath: trimmedPath,
-      queryStringObject: queryStringObject,
-      method: method,
-      headers: headers,
-      payload: helpers.parseJsonToObject(buffer),
-    };
-
-    chosenHandler(data, function (statuscode, payload) {
-      statuscode = typeof statuscode == "number" ? statuscode : 200;
-      payload = typeof payload == "object" ? payload : {};
-
-      //convert the payload to a string
-      var payloadString = JSON.stringify(payload);
-
-      //return the response
-      res.setHeader("Content-Type", "application/json");
-      res.writeHead(statuscode);
-      res.end(payloadString);
-
-      //log the request path
-      console.log("Returning this response: ", statuscode, payload);
-    });
-  });
+  // Start the workers
+  workers.init();
 };
 
-//Define a request router
-var router = {
-  ping: handlers.ping,
-  users: handlers.users,
-  tokens: handlers.tokens,
-  checks: handlers.checks,
-};
+// Self executing
+app.init();
+
+// Export the app
+module.exports = app;
